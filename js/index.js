b1af85e7d2ede077a41c98579a908a5d0340613f
bilibiligabe <118792602+bilibiligabe@users.noreply.github.com>
2023-01-01 15:30:40 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,19 +2,16 @@
 var tanchuang = Math.floor((Math.random() * 10) + 1);
 if(tanchuang < 5){
     setTimeout(function a () {
-        var tanchuang = document.getElementById("tanchuang");tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';var djs = 15;document.getElementById("djs").innerHTML= djs;
+        var tanchuang = document.getElementById("tanchuang");tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';var djs = 10;document.getElementById("djs").innerHTML= djs;
         setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 1000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 2000);
         setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 3000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 4000);
         setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 5000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 6000);
         setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 7000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 8000);
         setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 9000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 10000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 11000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 12000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 13000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 14000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 15000);setTimeout(function b () {tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';}, 15400);
-        setTimeout(function b () {tanchuang.style.display = "none";}, 15900);
+        setTimeout(function b () {tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';}, 10400);
+        setTimeout(function b () {tanchuang.style.display = "none";}, 10900);
     }, 1000);
 }
-
 //返回顶部
 function fhdb() {
     document.body.scrollTop = 0;
@@ -24,40 +21,22 @@ function fhdb() {
 function guanbi(guanbi) {
     guanbi.style.display = "none";
 }
-
-//答题
-function dati(){
-    var datiban = document.getElementById("datiban");
-    datiban.style.display = "block";
+//计算时间
+var nowtime = new Date;
+var time = nowtime.getTime();
+var time1 = time - 1672406400000; //TIME:2020/12/30/21:20:00
+var second = parseInt(time1 / 1000);
+var minute = second / 60;
+var hour = minute / 60;
+var day = parseInt(hour / 24);
+window.onload = function(){
+    document.getElementById("time").innerHTML="网站已运行："+day+"天";
 }
-function answer(){
-    var answer = document.getElementById("O3").value;
-    if(answer == 'O114514') {
-        alert('好臭啊（恼）');
-        document.getElementById("tongguotishi").innerHTML= "恭喜通过，抽奖中…";
-        setTimeout(function qiehuan () {
-            document.getElementById("tongguotishi").innerHTML= "恭喜获得价值666元红包<b>碎片</b>*99,已收集<b>99%</b>";
-        }, 2000);
-    } else if (answer == 'O3') {
-        alert('答对了');
-        document.getElementById("tongguotishi").innerHTML= "恭喜通过，抽奖中…";
-        setTimeout(function qiehuan () {
-            document.getElementById("tongguotishi").innerHTML= "恭喜获得价值666元红包<b>碎片</b>*99,已收集<b>99%</b>";
-        }, 2000);
-    }else {
-        alert('错误，请重试');
-    };
-}
-var A = 99;
-var B = 9;
-function huoqu(){
-    var answer = document.getElementById("O3").value;
-    if (answer !== 'O114514' && answer !== 'O3'){return;}
-    B = B/10;
-    A = A+B;
-    if (A > 99.99999999999991){
-        document.getElementById("tongguotishi").innerHTML= "发生”神秘“错误";
-        return;
-    }
-    document.getElementById("tongguotishi").innerHTML= "已收集<b>"+A+"%</b>";
+//菜单
+function caidan (){
+    var caidan = document.getElementById("caidan");
+    var height = caidan.offsetHeight;
+    if (height == 0){document.getElementById("caidan-guanbi").style.display="block";}
+    caidan.style.height = height===0 ? 350+'px' : 0+'px';
+    if (height == 350){document.getElementById("caidan-guanbi").style.display="none";}
 }
