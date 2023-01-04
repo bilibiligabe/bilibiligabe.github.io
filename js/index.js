0f8e15f5381fa6cf216accc332a578de49464496
bilibiligabe <118792602+bilibiligabe@users.noreply.github.com>
2023-01-04 11:09:53 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -2,12 +2,14 @@
 var tanchuang = Math.floor((Math.random() * 10) + 1);
 if(tanchuang < 5){
     setTimeout(function a () {
-        var tanchuang = document.getElementById("tanchuang");tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';var djs = 10;document.getElementById("djs").innerHTML= djs;
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 1000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 2000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 3000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 4000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 5000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 6000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 7000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 8000);
-        setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 9000);setTimeout(function b () {djs--;document.getElementById("djs").innerHTML= djs;}, 10000);
+        var tanchuang = document.getElementById("tanchuang");tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';
+        var djs = 10;var djsrq = document.getElementById("djs");
+        djsrq.innerHTML= djs;
+        setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 1000);setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 2000);
+        setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 3000);setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 4000);
+        setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 5000);setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 6000);
+        setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 7000);setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 8000);
+        setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 9000);setTimeout(function b () {djs--;djsrq.innerHTML= djs;}, 10000);
         setTimeout(function b () {tanchuang.style.height = tanchuang.offsetHeight===0 ? 230+'px' : 0+'px';}, 10400);
         setTimeout(function b () {tanchuang.style.display = "none";}, 10900);
     }, 1000);
@@ -24,7 +26,7 @@ function guanbi(guanbi) {
 //计算时间
 var nowtime = new Date;
 var time = nowtime.getTime();
-var time1 = time - 1672406400000; //TIME:2020/12/30/21:20:00
+var time1 = time - 1672502400000; //TIME：2023/1/1/00:00:00
 var second = parseInt(time1 / 1000);
 var minute = second / 60;
 var hour = minute / 60;
@@ -32,7 +34,16 @@ var day = parseInt(hour / 24);
 window.onload = function(){
     document.getElementById("time").innerHTML="网站已运行："+day+"天";
 }
-//菜单
+//菜单1
+function dncdzk(){
+    var dncd = document.getElementById("dncd");
+    dncd.style.height = 300+"px";
+}
+function dncdgb(){
+    var dncd = document.getElementById("dncd");
+    dncd.style.height = 0+'px';
+}
+//菜单2
 function caidan (){
     var caidan = document.getElementById("caidan");
     var height = caidan.offsetHeight;
