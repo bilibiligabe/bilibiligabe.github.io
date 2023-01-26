9a350ea422ecccd7d2405ef5a2bd2eb6b1f4b20f
bilibiligabe <118792602+bilibiligabe@users.noreply.github.com>
2023-01-26 15:55:51 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+//创建数组
+var cookies=document.cookie.split("; ");  //把cookie自带的"; "存为数组并改为","
+if(cookies!==""){
+    console.log(cookies);  //输出
+    window.onload = function(){document.getElementById("username").innerHTML=cookies[0];}
+}
+
 //随机弹窗
 var tanchuang = Math.floor((Math.random() * 10) + 1);
 if(tanchuang < 5){
@@ -36,7 +43,6 @@ function time() {
     var day = parseInt(hour / 24);
     window.onload = function(){
         document.getElementById("time").innerHTML="网站已运行："+day+"天";
-        document.getElementById("time1").innerHTML="网站已运行："+day+"天";
     }
 }
 //菜单1
@@ -56,4 +62,32 @@ function caidan (){
     document.getElementById("caidan-guanbi").style.display="block";
     setTimeout(function a () {caidan.style.height = height===0 ? 420+'px' : 0+'px';},100);
     if (height == 420){setTimeout(function a () {caidan.style.display="none";}, 400);}
+}
+//Sign in
+function sign (){
+    document.getElementById("denglu").style.display = "block";
+}
+//From and Cookie
+function usernews (){
+    //setCookie
+    var username = "user="+document.getElementById("username").value+";";
+    var mail = "mail="+document.getElementById("mail").value+";";
+    var password = "password="+document.getElementById("password").value+";";
+    var expires ="expires=Sun Feb 18 2024 09:00:00 GMT;";
+    if(document.getElementById("username").value == ""){alert('输入用户名！');return;}
+    if(document.getElementById("mail").value == ""){alert('输入邮箱！');return;}
+    if(document.getElementById("password").value == ""){alert('输入密码！');return;}
+    document.cookie = mail+ expires +"path=/";
+    document.cookie = username + expires +"path=/";
+    document.cookie = password + expires +"path=/";
+    //from
+    document.getElementById("form").submit();
+    //close
+    document.getElementById("denglu").style.display = "none";
+    
+    //getCookie
+    var cookies=document.cookie.split("; ");  //把cookie自带的"; "存为数组并改为","
+    console.log(cookies);  //输出
+    //sign in
+    document.getElementById("username").innerHTML=cookies[0];
 }
