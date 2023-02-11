66164c1346250ed1f9c3252ccfded50295f3cdda
bilibiligabe <118792602+bilibiligabe@users.noreply.github.com>
2023-02-12 07:58:06 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ if(cookies === ""){
 //验证浏览器
 var userAgent = window.navigator.userAgent.match("DingTalk");
 if(userAgent !== null){
-    alert("不要用钉钉浏览器或QQ浏览器打开");
+    setInterval(function (){alert("不要用钉钉浏览器或QQ浏览器打开");},2000);
 }else {
     console.log("浏览器支持");
 }
